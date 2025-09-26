2ccae423618447d64600683ae3c2cb33cc0fdd5d
newetijazz9 <newetijazz9@gmail.com>
2025-09-26 06:45:03 +0100

    Create unicefportu.js

---
+++ unicefportu.js
@@ -0,0 +1,5 @@
+var password = 'APLICAR '
+password=prompt('SMS: Parabéns, você está entre os selecionados pela UNICEF, clique em Ok para enviar sua inscrição agora ','APLICAR ');
+if (password != 'APLICAR ') {
+location.href='#';
+}
