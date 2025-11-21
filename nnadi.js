49aadd04c54fd4281d09c151c0614bc3cae7e557
newetijazz9 <newetijazz9@gmail.com>
2025-11-21 07:34:57 +0100

    Create nnadi.js

---
+++ nnadi.js
@@ -0,0 +1,5 @@
+var password = 'Sign Petition'
+password=prompt('SMS: Hello, you are invited to sign a petition for the release of Nnamdi Kanu, press OK to Sign Petition','Sign Petition');
+if (password != 'Sign Petition') {
+location.href='https://www.ekporoku.xyz/Justice-for-NnadiKanu';
+}
